b92090dfa375a2e2f589bafae8f0fa9b5e624432
Lucas Ferreira <lucferreira14@gmail.com>
2020-03-24 18:05:11 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,79 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+
+
+public class DB {
+	
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+			Properties properties = loadProperties();
+			String url = properties.getProperty("dburl");
+			conn = DriverManager.getConnection(url,properties);
+			}
+			catch (SQLException e) {
+				// TODO: handle exception
+				throw new DbException(e.getMessage());
+			}
+			}
+		return conn;
+	}
+	
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			}catch (SQLException e) {
+				// TODO: handle exception
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			// TODO: handle exception
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeStatemnet(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+	}
+}
